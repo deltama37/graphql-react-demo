d8ae95fc3af47ecdcd4d05e8b93a498b31ad122e
ryota misumi <3delta7@gmail.com>
2022-10-26 05:16:33 +0900

    format code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <div >
-      aa
-    </div>
-  )
-}
+  return <div>aa</div>;
+};
 
-export default Home
+export default Home;
